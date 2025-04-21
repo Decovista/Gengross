21d25410b3404870382b0218bd1ea5e9debd5ef4
Decovista <sales.decovista@gmail.com>
2025-04-21 13:36:49 +0530

    added something new

--- src/Components/Contact-form/Contact.jsx
+++ src/Components/Contact-form/Contact.jsx
@@ -1,27 +1,57 @@
-import React from 'react'
-import ContactPopup from '../Contact-popup/ContactPopup'
-import './Contact.css'
+import React from 'react';
+import ContactPopup from '../Contact-popup/ContactPopup';
+import './Contact.css';
 
-function Contact({setShowContact}) {
+function Contact({ setShowContact }) {
   return (
     <div className='Contact'>
-        <h2>Connect with US</h2>
-        <div className="card-wrapper">
+      <h2>Connect with Us</h2>
+      <div className="card-wrapper">
         <ul className="cards">
-        <h3>Contact Details</h3>
-        <li><i class="fa-solid fa-location-dot"></i><p>Vipul bussiness Park</p></li>
-        <li><i class="fa-solid fa-location-dot"></i><p>Gurgram || Patna || Bihar || NCR </p></li>
-        <li><i class="fa-solid fa-phone"></i><p>9708954645</p></li>
-        <li><i class="fa-regular fa-envelope"></i><p>gengross.lifescience@gmail.com</p></li>
-      </ul>
-      <div className="cards">
-        <h3>Get-In Touch</h3>
-        <p>Kindly click on the button and fill the contact form to get our Services</p>
-        <button onClick={() => setShowContact(true) }>Subscribe Us</button>
-      </div>
+          <h3>Contact Details</h3>
+          <li>
+            <i className="fa-solid fa-location-dot"></i>
+            <a
+            href="https://www.google.co.in/maps/place/Vipul+Business+Park,+Central+Park+II+Rd,+Vipul+World,+Sector+48,+Gurugram,+Haryana+122004/@28.4226504,77.035029,17z/data=!3m1!4b1!4m6!3m5!1s0x390d18758a93762b:0x9348ae09f4ba0007!8m2!3d28.4226457!4d77.0376039!16s%2Fg%2F1pt_f2nln?entry=ttu&g_ep=EgoyMDI1MDQxNi4xIKXMDSoJLDEwMjExNDU1SAFQAw%3D%3D"
+              target="_blank"
+               rel="noopener noreferrer"
+               >
+            <p>Gengross Life Sciencs</p> </a>
+          </li>
+          <li>
+            <i className="fa-solid fa-location-dot"></i>
+            <a
+            href="https://www.google.co.in/maps/place/Vipul+Business+Park,+Central+Park+II+Rd,+Vipul+World,+Sector+48,+Gurugram,+Haryana+122004/@28.4226504,77.035029,17z/data=!3m1!4b1!4m6!3m5!1s0x390d18758a93762b:0x9348ae09f4ba0007!8m2!3d28.4226457!4d77.0376039!16s%2Fg%2F1pt_f2nln?entry=ttu&g_ep=EgoyMDI1MDQxNi4xIKXMDSoJLDEwMjExNDU1SAFQAw%3D%3D"
+              target="_blank"
+               rel="noopener noreferrer"
+               >
+               Central Park II Rd, Vipul World, Sector 48, Gurugram, Haryana 122004
+              </a>
+          </li>
+          <li>
+            <i className="fa-solid fa-phone"></i>
+            <p>
+              <a href="tel:+919708954645">+91 97089 54645</a>
+            </p>
+          </li>
+          <li>
+            <i className="fa-regular fa-envelope"></i>
+            <p>
+              <a href="mailto:gengross.lifescience@gmail.com">
+                gengross.lifescience@gmail.com
+              </a>
+            </p>
+          </li>
+        </ul>
+
+        <div className="cards">
+          <h3>Get In Touch</h3>
+          <p>Kindly click on the button and fill the contact form to get our services.</p>
+          <button onClick={() => setShowContact(true)}>Subscribe Us</button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
